609909866aaf0e4abe5d60819bf830933ed53580
wwd <ericrock@foxmail.com>
2016-12-19 10:52:56 +0800

    build: Skip buildroot even though platform is linux

--- buildroot/scripts/build.sh
+++ buildroot/scripts/build.sh
@@ -116,12 +116,12 @@ case "$1" in
         rm -rf ${LICHEE_BR_OUT}
         ;;
     *)
-        if [ "x${LICHEE_PLATFORM}" = "xlinux"  -o "x${LICHEE_PLATFORM}" = "xeyeseelinux" ] ; then
-            build_buildroot
-            export PATH=${LICHEE_BR_OUT}/external-toolchain/bin:$PATH
-            build_external
-        else
+        # if [ "x${LICHEE_PLATFORM}" = "xlinux"  -o "x${LICHEE_PLATFORM}" = "xeyeseelinux" ] ; then
+        #     build_buildroot
+        #     export PATH=${LICHEE_BR_OUT}/external-toolchain/bin:$PATH
+        #     build_external
+        # else
             build_toolchain
-        fi
+        # fi
         ;;
 esac
